ecb594c1268bab1325a3f2150911982755c10b4a
Andrey131 <36641490+Andrey131@users.noreply.github.com>
2018-03-12 18:22:45 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
-module.exports = function longestConsecutiveLength(array) {
-  // your solution here
+module.exports = function longestConsecutiveLength(array) 
+{
+
+	if(array.length == 0)
+	{
+		return 0;
+	}
+
+	if(array.length == 1)
+	{
+		return 1;
+	}
+
+	let MaxLength = 1;
+	let Temp= 1;
+
+	for(let i = 0; i < array.length; i++)
+	{
+		if((array[i] + 1 == array[i+1]))
+		{
+			Temp++;
+			MaxLength = Math.max(MaxLength,Temp);
+		}
+		else 
+		{
+			if(array[i] != array[i+1])
+			{
+				Temp = 1;
+			}
+		}
+	}
+
+	return MaxLength;
+
 }
